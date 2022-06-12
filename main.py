9bf720b681817fec36170b325fbc6141a59cebb9
zhi <mzyxlzl@163.com>
2022-06-12 10:51:51 +0800

    ssh-git

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print('===========Hello!---------$$$$$$$$$')
+    print('===========Hello!---------$$$$$$$$$-----ssh-git push')
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
